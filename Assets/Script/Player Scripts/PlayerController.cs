29cf84337b22200360fa772670d6b7ca534a9991
Ajay <57134829+Ajayuthaman@users.noreply.github.com>
2023-07-24 14:14:16 +0530

    Player Movement
    Added basic movement for player

---
+++ Assets/Script/Player Scripts/PlayerController.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 5f;
+
+    public float minY, maxY;
+
+    private void Start()
+    {
+        
+    }
+
+
+    private void Update()
+    {
+        MovePlayer();
+    }
+
+
+    void MovePlayer()
+    {
+        if (Input.GetAxisRaw("Vertical") > 0f)
+        {
+            Vector3 temp = transform.position;
+            temp.y += speed * Time.deltaTime;
+
+            if (temp.y > maxY)
+                temp.y = maxY;
+
+            transform.position = temp;
+
+        }
+        else if (Input.GetAxisRaw("Vertical") < 0f)
+        {
+            Vector3 temp = transform.position;
+            temp.y -= speed * Time.deltaTime;
+
+            if (temp.y < minY)
+                temp.y = minY;
+
+            transform.position = temp;
+        }
+    }
+}
